603999cbd862160adefdfa23bd5384be926a7b0b
Alex Scott <codetheorist@gmail.com>
2017-07-22 00:34:58 +0100

    Add Magellan component

---
+++ src/components/Magellan.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="row">
+    <div class="medium-10 medium-offset-1 columns">
+      <h1>{{ msg }}</h1>
+      <ul id="magellan" class="horizontal menu" data-magellan>
+        <li><a href="#first">First</a></li>
+        <li><a href="#second">Second</a></li>
+        <li><a href="#third">Third</a></li>
+      </ul>
+      <div class="sections">
+        <section id="first" data-magellan-target="first"><span>First Section</span></section>
+        <section id="second" data-magellan-target="second"><span>Second Section</span></section>
+        <section id="third" data-magellan-target="third"><span>Third Section</span></section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'magellan',
+  mounted () {
+    this.magellan = new Foundation.Magellan($('#magellan'), {
+      // These options can be declarative using the data attributes
+      animationEasing: 'swing'
+    })
+  },
+  data () {
+    return {
+      msg: 'Magellan'
+    }
+  },
+  destroyed () {
+    this.magellan.destroy()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  section {
+    min-height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 1rem;
+    background: $light-gray;
+  }
+
+  span {
+    font-size: 2rem;
+    font-weight: 300;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Tabs from '@/components/Tabs'
 import Tooltip from '@/components/Tooltip'
+import Magellan from '@/components/Magellan'
 import Orbit from '@/components/Orbit'
 import Reveal from '@/components/Reveal'
 import Slider from '@/components/Slider'
@@ -17,6 +18,7 @@ export default new Router({
     { name: 'Hello', path: '/', component: Hello },
     { name: 'accordion', path: '/accordion', component: Accordion },
     { name: 'dropdown', path: '/dropdown', component: Dropdown },
+    { name: 'magellan', path: '/magellan', component: Magellan },
     { name: 'orbit', path: '/orbit', component: Orbit },
     { name: 'reveal', path: '/reveal', component: Reveal },
     { name: 'slider', path: '/slider', component: Slider },
