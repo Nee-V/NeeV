583d84e5e3553f542ea0f4d3e3201d9608c43b5a
Alex Scott <codetheorist@gmail.com>
2017-07-22 02:39:02 +0100

    Add props to Accordion component for Javascript options

--- src/components/core/Accordion.vue
+++ src/components/core/Accordion.vue
@@ -2,7 +2,17 @@
   <div class="row">
     <div class="medium-10 medium-offset-1 columns">
       <h1>{{ msg }}</h1>
-      <ul id="accordion" class="accordion" data-accordion>
+      <ul
+        id="accordion"
+        class="accordion"
+        data-accordion
+        v-bind:data-slide-speed="speed"
+        v-bind:data-multi-expand="multi"
+        v-bind:data-allow-all-closed="allClosed"
+        v-bind:data-deep-link="dL"
+        v-bind:data-deep-link-smudge="dLSmudge"
+        v-bind:data-deep-link-smudge-delay="dLSmudgeDelay"
+        v-bind:data-update-history="history">
         <li class="accordion-item is-active" data-accordion-item>
           <a href="#" class="accordion-title">Accordion 1</a>
           <div class="accordion-content" data-tab-content>
@@ -24,15 +34,48 @@
 export default {
   name: 'accordion',
   mounted () {
-    this.accordion = new Foundation.Accordion($('#accordion'), {
-      // These options can be declarative using the data attributes
-      slideSpeed: 250,
-      multiExpand: false
-    })
+    this.accordion = new Foundation.Accordion($('#accordion'))
   },
   data () {
     return {
-      msg: 'Accordion'
+      msg: 'Accordion',
+      speed: this.slideSpeed ? this.slideSpeed : 250,
+      multi: this.multiExpand ? this.multiExpand : false,
+      allClosed: this.allowAllClosed ? this.allowAllClosed : false,
+      dL: this.deepLink ? this.deepLink : false,
+      dLSmudge: this.deepLinkSmudge ? this.deepLinkSmudge : false,
+      dLSmudgeDelay: this.deepLinkSmudgeDelay ? this.deepLinkSmudgeDelay : 300,
+      history: this.updateHistory ? this.updateHistory : false
+    }
+  },
+  props: {
+    slideSpeed: {
+      type: Number,
+      default: () => 250
+    },
+    multiExpand: {
+      type: Boolean,
+      default: () => false
+    },
+    allowAllClosed: {
+      type: Boolean,
+      default: () => false
+    },
+    deepLink: {
+      type: Boolean,
+      default: () => false
+    },
+    deepLinkSmudge: {
+      type: Boolean,
+      default: () => false
+    },
+    deepLinkSmudgeDelay: {
+      type: Number,
+      default: () => 300
+    },
+    updateHistory: {
+      type: Boolean,
+      default: () => false
     }
   },
   destroyed () {
