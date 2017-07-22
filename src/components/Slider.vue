6f7e129b48767329f4aeefbd853e33c55554f700
Alex Scott <codetheorist@gmail.com>
2017-07-22 01:13:07 +0100

    Add ID prop to Slider component

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -3,14 +3,14 @@
     <div class="medium-10 medium-offset-1 columns">
       <h1>{{ msg }}</h1>
       <div
-        id="slider"
+        v-bind:id="this.sliderID"
         v-bind:data-vertical="isVertical === true ? 'true' : 'false'"
         v-bind:class="[isVertical === true ? 'vertical' : '', isDisabled === true ? 'disabled' : '']"
         class="slider "
         data-slider
         v-bind:data-initial-start="this.dataValue"
         v-bind:data-end="this.dataEnd"
-        v-bind:data-step="stepValue"
+        v-bind:data-step="this.stepValue"
         >
         <span class="slider-handle" data-slider-handle role="slider" tabindex="1"></span>
         <span class="slider-fill" data-slider-fill></span>
@@ -25,7 +25,7 @@
 export default {
   name: 'slider',
   mounted () {
-    this.slider = new Foundation.Slider($('#slider'), {
+    this.slider = new Foundation.Slider($('#' + this.sliderID), {
       // These options can be declarative using the data attributes
     })
     this.slider.$element.on('moved.zf.slider', () => {
@@ -39,7 +39,8 @@ export default {
       dataEnd: 200,
       isVertical: this.verticalLayout ? this.verticalLayout : false,
       isDisabled: this.disabled ? this.disabled : false,
-      stepValue: this.stepSize ? this.stepSize : 5
+      stepValue: this.stepSize ? this.stepSize : 5,
+      sliderID: this.sliderId ? this.sliderId : 'slider'
     }
   },
   props: {
@@ -58,6 +59,10 @@ export default {
     stepSize: {
       type: Number,
       default: () => 20
+    },
+    sliderId: {
+      type: String,
+      default: () => 'slider'
     }
   },
   destroyed () {
