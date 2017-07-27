f3d513b16b7ff0936cae0a7576d5b96d5d0bfe1f
Alex Scott <codetheorist@gmail.com>
2017-07-27 20:02:54 +0000

    Add docs/ folder again

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../docs/index.html'),
     assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: 'NeeV/static',
+    assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
